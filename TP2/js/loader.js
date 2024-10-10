621947128c44206d40007af07167a062ac8781b2
Luciano <tluciano943@gmail.com>
2024-10-10 12:14:59 -0300

    Loader completo terminado

--- TP2/js/loader.js
+++ TP2/js/loader.js
@@ -3,9 +3,23 @@ const header = document.querySelector(".header");
 const container = document.querySelector(".container");
 const footer = document.querySelector("footer");
 
+const loader_number = document.getElementById("loader__number");
+let counter = 0;
+
 setTimeout(() => {
     loader.classList.add("style__loader__visibility");
     header.classList.add("styles__eliminated__for__loader");
     container.classList.add("styles__eliminated__for__loader");
     footer.classList.add("styles__eliminated__for__loader");
 }, 5000);
+
+const load = setInterval(() => {
+    console.log("ENTRE");
+    if(counter < 100) {
+        counter++;
+        loader_number.innerHTML = `${counter}%`;
+    } else {
+        clearInterval(load);
+    }
+}, 47);
+
